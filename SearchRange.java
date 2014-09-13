c59d52013448258666147949a66478626e6d4b08
dian.wen <2010wendian@gmail.com>
2014-09-13 15:06:09 -0400

    bug fixed SearchRange & commented

--- SearchRange.java
+++ SearchRange.java
@@ -1,14 +1,31 @@
+/**
+*Use Binary Search to narrow the range
+*Step 1 : To find the left bound of target
+*	key point is if A[mid] == target
+*	let end = mid;
+*	keep search left
+*Step 2 : To find the right bound of target
+*	key point is if A[mid] == target
+*	let start = mid;
+*	keep search right
+*
+*
+*/
 public class Solution {
-    public int[] searchInsert(int[] A, int target) {
+    public int[] searchRange(int[] A, int target) {
         int[] bound = new int[2];
         int start, mid, end;
         
+        //check empty case;
         if (A == null || A.length == 0) {
             bound[0] = bound[1] = -1;
             return bound;
         }
         
         //looking for left bound;
+		//key point is if A[mid] == target
+		//let end = mid;
+		//keep search left;
         start = 0;
         end = A.length - 1;
         while (start + 1 < end) {
@@ -21,7 +38,11 @@ public class Solution {
         		end = mid;
         	}
         }
-
+        //above are Binary Search Template
+        //after above start + 1 = end; 
+        //there are only 2 possible situation
+        //start = target || end = target
+        //target doesn't exist
         if (A[start] == target) {
         	bound[0] = start;
         }else if (A[end] == target) {
@@ -32,12 +53,15 @@ public class Solution {
         }
 
         //looking for right bound
+		//key point is if A[mid] == target
+		//let start = mid;
+		//keep search right
         start = 0;
         end = A.length - 1;
         while (start + 1 < end) {
         	mid = start + (end - start) / 2;
         	if (A[mid] == target) {
-        		end = mid; 
+        		start = mid; 
         	}else if (A[mid] < target) {
         		start = mid;
         	}else {
@@ -45,6 +69,11 @@ public class Solution {
         	}
         }
 
+        //above are Binary Search Template
+        //after above start + 1 = end; 
+        //there are only 2 possible situation
+        //start = target || end = target
+        //target doesn't exist
         if (A[end] == target) {
         	bound[1] = end;
         }else if (A[start] == target) {
