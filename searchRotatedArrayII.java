9137fa30a8db1256d6ece61dc9acb62f42e2a014
dian.wen <2010wendian@gmail.com>
2014-09-15 00:55:16 -0400

    bug fixed searchRotatedArrayII with Comment solution

--- searchRotatedArrayII.java
+++ searchRotatedArrayII.java
@@ -1,8 +1,17 @@
 /**
 *Thoughts mostly same to Search Rotated Array
 *Key point: To find skip the duplicates numbers
-*Diffecence: if A[start] >= A[mid] does NOT mean A
-*
+*Diffecence: if A[start] >= A[mid] does NOT mean A[mid] to A[start] in order
+*Method: split every single situation
+*   A[start] == A[mid] start ++; skip the duplicates
+*   A[start] > A[mid] 
+*       search target if among A[mid]-A[end] (suppose mid - end ordered)
+*   if not
+*       end = mid search back
+*   A[start] < mid
+*       search target if among A[start]-A[mid] (suppose start - mid ordered)
+*   if not
+*       start = mid search back
 */
 
 public class Solution {
@@ -19,6 +28,7 @@ public class Solution {
             if (A[mid] == target) {
                 return true;
             }else if (A[start] == A[mid]) {
+                //key point: skip the duplicates
                 start++;
             }else if (A[start] > A[mid]){
                 if (A[mid] <= target && target <= A[end]) {
