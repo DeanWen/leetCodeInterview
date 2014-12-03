dc86bd69bb6c02d8676e0f6bd054f29558f059dc
dian.wen <2010wendian@gmail.com>
2014-12-02 22:09:55 -0500

    update AddBinaryMethod and Reverse Integer

--- AddTwoNumberMethod2.java
+++ AddTwoNumberMethod2.java
@@ -3,6 +3,12 @@
  * 进入循环，只要L1或L2不为null或carry不为0，
  * 构建新node加入dummy链表尾部，更新carry。
  * Key(在循环内部分类讨论)
+ *细节和陷阱： 
+ *1）在proceed(l = l.next)之前要先判断是否为空 （陷阱）
+ *2）结尾处如果carry还有值，要新建一个节点并连接到链表尾部。- Q! X  l3 R' T$ [$ k3 G! E$ _
+ *3）用了dummy头节点。
+ * Overall Time Complexity is O(N)
+ * Overall Space Complexity is O(N)
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
