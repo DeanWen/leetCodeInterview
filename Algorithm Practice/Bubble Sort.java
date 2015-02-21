634089187bffb032a7704479bb38022276ef6ae5
dian.wen <2010wendian@gmail.com>
2015-02-21 17:58:13 -0500

    clean up name normalization

--- Algorithm Practice/Bubble Sort.java
+++ Algorithm Practice/Bubble Sort.java
@@ -1,15 +1,52 @@
-/*
-* Bubble Sort
+/**
+* bubble sort
 */
 
-public static void bubbleSort (int[] array) {
-	for (int i = 0; i < array.length; i++) {
-		for (int j = 0; j < array.length - i - 1; j++ ) {
-			if (array[j] > array[j + 1]) {
-				int temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] array = new int [args.length];
+
+		for(int i = 0; i < args.length; i++) {
+			array[i] = Integer.parseInt(args[i]);
+		}
+
+		printOut(bubbleSortAscending(array));
+		System.out.println();
+		printOut(bubbleSortDescending(array));
+		System.out.println();
+	}
+
+	public static void printOut(int[] array){
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i]+ " ");
+		}
+	}
+
+	public static int[] bubbleSortAscending(int[] array) {
+		for (int i = 0; i < array.length; i++ ) {
+			for (int j = 1; j < array.length - i; j++){
+				if (array[j-1] > array[j]) {
+					int temp = array[j-1];
+					array[j-1] = array[j];
+					array[j] = temp;
+				}
 			}
 		}
+
+		return array;
+	}
+
+	public static int[] bubbleSortDescending(int[] array) {
+		for (int i = 0; i < array.length; i++ ) {
+			for (int j = 1; j < array.length - i; j++){
+				if (array[j-1] < array[j]) {
+					int temp = array[j-1];
+					array[j-1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+
+		return array;
 	}
 }
--- Algorithm Practice/HashTable Implementation.java
+++ Algorithm Practice/HashTable Implementation.java
@@ -1,3 +1,14 @@
+/*
+Difference among 
+HashSet HashMap HashTable
+
+HashSet only store the object: add() hasCode() equals()
+	can not store duplicate object
+HashMap store <key, object> : put() get() containsKey() 
+	can store duplicate value iff key is different
+HashTable almost same with HashMap, but is syncronized and not accept null value.
+*/
+
 public class HashEntry {
 	private int key;
 	private int value;
