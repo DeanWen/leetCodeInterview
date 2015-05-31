5963257fb57420993257dcfa58a9b8c06615b02b
dian.wen <2010wendian@gmail.com>
2015-05-31 16:30:50 -0400

    relocate

--- Leet Code/29-Divide Two Integers.java
+++
@@ -1,4 +0,0 @@
-/*
-* http://blog.csdn.net/linhuanmars/article/details/20024907
-*/
-
--- LeetCode/79-Word Search.java
+++ LeetCode/79-Word Search.java
@@ -1,43 +1,44 @@
 public class Solution {
-	public boolean exist(char[][] board, String word) { 
-		if (board == null || board.length == 0 || board[0].length == 0) {
-			return false;
-		}
-
-		int m = board.length;
-		int n = board.length;
-		boolean[][] visited = new boolean[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (dfs(board, i, j, 0, word, visited)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	public boolean dfs (char[][] board, int i, int j, 
-			int index, String word, boolean[][] visited) {
-		if (index == word.length()) {
-			return true;
-		}
-
-		if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
-			return false;
-		}
-
-		if (board[i][j] != word.charAt(index) || visited[i][j]) {
-			return false;
-		}
-
-		visited[i][j] = true;
-		boolean res = dfs(board, i + 1, j, index + 1, word, visited) ||
-					  dfs(board, i - 1, j, index + 1, word, visited) ||
-					  dfs(board, i, j + 1, index + 1, word, visited) ||
-					  dfs(board, i, j - 1, index + 1, word, visited);
-		visited[i][j] = false;
-		return res;
-	}
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (helper(board, i, j, word, 0, visited)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean helper(char[][] board, int i, int j, String word, int start, boolean[][] visited) {
+        if (start == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return false;
+        }
+        if (board[i][j] != word.charAt(start)) {
+            return false;
+        }
+        if (visited[i][j]) {
+            return false;
+        }
+        
+        visited[i][j] = true;
+        boolean result = helper(board, i + 1, j, word, start + 1, visited) ||
+                          helper(board, i - 1, j, word, start + 1, visited) ||
+                          helper(board, i, j + 1, word, start + 1, visited) || 
+                          helper(board, i, j - 1, word, start + 1, visited);
+        visited[i][j] = false;
+        return result;
+    }
+    
 }
