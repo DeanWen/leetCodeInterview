dea1ad5dbeb08ff6f6dc92bfa8825af02fc9d542
dian.wen <2010wendian@gmail.com>
2015-06-16 22:27:14 -0400

    bug fix

--- Groupon/Find Related Coffee.java
+++ Groupon/Find Related Coffee.java
@@ -1,29 +1,31 @@
 //given object class
 public class Coffee {}
+//given function
+public double getScore(Coffee A, Coffee B) {}
 
 //helper class to match coffee and similarity score
 public class Pair {
-  Coffee coffee;
-  double score;
-  Pair (Coffee c, double s) {
-    coffee = c;
-    score = s;
-  }
+    Coffee coffee;
+    double score;
+    Pair (Coffee c, double s) {
+      coffee = c;
+      score = s;
+    }
 }
 
 public List<Coffee> getTopK(List<Coffee> menu, Coffee A, int size) {
     List<Pair> scorelist = new ArrayList<Pair>();
     
-  //O(n) to get similarity score
+    //O(n) to get similarity score
     for (Coffee c : menu) {
       Pair p = new Pair (c, getScore(A, c));
       scorelist.add(p);
     }
   
     final int recommendationSize = size;
-    PriorityQueue<Double> minHeap = new PriorityQueue<Double>(recommendationSize, new myComparator());
+    PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>(recommendationSize, new myComparator());
     
-  //O(nlogk) k = size to get the top k for minHeap
+    //O(nlogk) k = size to get the top k for minHeap
     for (Pair p : scorelist) {
        minHeap.offer(p);
        if (minHeap.size() >= reommendationSize + 1) {
@@ -31,7 +33,7 @@ public List<Coffee> getTopK(List<Coffee> menu, Coffee A, int size) {
        }
     }
     
-  //O(1) to add coffee to result list
+    //O(1) size is constant to add coffee to result list
     List<Coffee> result = new List<Coffee>();
     for (Pair p : minHeap) {
       result.add(p.coffee);
@@ -42,11 +44,8 @@ public List<Coffee> getTopK(List<Coffee> menu, Coffee A, int size) {
 
 //customized comparator
 public class myComparator<Pair> implements Comparator<Pair> {
-  @Override
-  public int compare (Pair a, Pair b) {
-    return a.score - b.score;
-  }
+    @Override
+    public int compare (Pair a, Pair b) {
+      return a.score - b.score;
+    }
 }
-
-//given function
-public double getScore(Coffee A, Coffee B) {}
