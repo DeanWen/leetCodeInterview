48e75000101bfc76377e688acba4e4338179fda4
Dean <dwen@cmu.edu>
2016-11-03 23:31:53 -0700

    Update 029-Divide Two Integers.java

--- LeetCode/029-Divide Two Integers.java
+++ LeetCode/029-Divide Two Integers.java
@@ -1,4 +1,21 @@
 /*
-* http://blog.csdn.net/linhuanmars/article/details/20024907
+* https://discuss.leetcode.com/topic/45980/very-detailed-step-by-step-explanation-java-solution/2
 */
+
+public int divide(int dividend, int divisor) {
+    boolean isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? true : false;
+    long absDividend = Math.abs((long) dividend);
+    long absDivisor = Math.abs((long) divisor);
+    long result = 0;
+    while(absDividend >= absDivisor){
+        long tmp = absDivisor, count = 1;
+        while(tmp <= absDividend){
+            tmp <<= 1;
+            count <<= 1;
+        }
+        result += count >> 1;
+        absDividend -= tmp >> 1;
+    }
+    return  isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
+}
 
