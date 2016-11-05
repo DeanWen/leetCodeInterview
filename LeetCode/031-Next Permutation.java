29e4ae8843a7bad57c26d5efa032d134d76a4786
Dean <dwen@cmu.edu>
2016-11-05 12:25:57 -0700

    Update 031-Next Permutation.java

--- LeetCode/031-Next Permutation.java
+++ LeetCode/031-Next Permutation.java
@@ -1,40 +1,36 @@
 /*
-*	http://blog.csdn.net/m6830098/article/details/17291259
+*	https://leetcode.com/articles/next-permutation/
 *	Time Complexity : O(n) actually O(3*n)
 *	Space Complexity: O(1)
 */
 public class Solution {
     public void nextPermutation(int[] nums) {
- 		if (nums == null || nums.length == 0) {
- 			return;
- 		}
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
 
- 		int i = nums.length - 2;
- 		while (i >= 0 && nums[i] >= nums[i + 1]) {
- 			i--;
- 		}
- 		
- 		if (i >= 0) {
-	 		int pos = i;
-	 		int j = i + 1;
-	 		while (j < nums.length && nums[j] > nums[pos]) {
-	 			j++;
-	 		}
-	 		swap(nums, pos, j - 1);
- 		}
- 		
- 		int left = i + 1;
- 		int right = nums.length - 1;
- 		while (left < right) {
- 			swap(nums, left, right);
- 			left++;
- 			right--;
- 		}
- 	}
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 
-    private void swap (int[] nums, int a, int b) {
-    	int temp = nums[a];
-    	nums[a] = nums[b];
-    	nums[b] = temp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
