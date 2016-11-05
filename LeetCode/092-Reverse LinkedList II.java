aa49851d7b63fc3d825308c2074ca25683d860cf
Dean <dwen@cmu.edu>
2016-11-05 00:25:19 -0700

    Update 092-Reverse LinkedList II.java

--- LeetCode/092-Reverse LinkedList II.java
+++ LeetCode/092-Reverse LinkedList II.java
@@ -1,68 +1,34 @@
 //Reverse a linked list from m to n
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode dummyNode = new ListNode(0);
-        dummyNode.next = head;
-        tail = head;
-        int step = n - m;
-        while(m > 0){
-            tail = tail.next;
-            m = m - 1;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        prev.next = head;
+        int i = 0;
+        while (head != null) {
+            if (i + 1 < m) {
+                prev = prev.next;
+            }
+            if (i == n) {
+                break;
+            }
+            head = head.next;
+            i++;
         }
         
-        ListNode prev,current,next;
-        prev = null;
-        current = tail;
-        next = current.next;
-        while(step > 0 && next != null) {
-            prev = current;
-            current = next;
-            next = prev;
-            step = step - 1;
-        }
-
-        tail.next = prev;
-
-        return dummyNode.next; 
+        prev = reverse(prev, head);
+        return dummy.next;
     }
-}
-
-
-//Reverse a linked list from m to n
-//Aug 24 Bug Free
-public class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        
-        ListNode dummyNode = new ListNode(0);
-        dummyNode.next = head;
-        int step = n - m;
-
-        if (step == 0){
-            return dummyNode.next;
-        }else{
-            ListNode prevX = dummyNode;
-            
-            while (m - 1> 0) {
-                prevX = prevX.next;
-                m = m - 1;
-            }
-
-            ListNode startPoint = prevX.next;
-            ListNode prev = prevX.next;
-            ListNode curr = prev.next;
-
-            while (step > 0) {
-                ListNode temp = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = temp;
-                step = step - 1;
-            }
-
-            prevX.next = prev;
-            startPoint.next = curr;
-
-            return dummyNode.next;
+    
+    private ListNode reverse(ListNode prev, ListNode end) {
+        ListNode last = prev.next;
+        ListNode curr = last.next;
+        while (curr != end) {
+            last.next = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+            curr = last.next;
         }
+        return last;
     }
 }
