2c2070d528f14931266d879775b803991d1039c7
Dean <dwen@cmu.edu>
2016-11-06 22:10:07 -0800

    Update 082-Remove Duplicates from Sorted List II.java

--- LeetCode/082-Remove Duplicates from Sorted List II.java
+++ LeetCode/082-Remove Duplicates from Sorted List II.java
@@ -1,25 +1,24 @@
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    	// use two pointers, slow - track the node before the dup nodes, 
+    	// fast - to find the last node of dups.
+        ListNode dummy = new ListNode(0);
+        ListNode fast = head;
+        ListNode slow = dummy;
+        slow.next = fast;
+        while(fast != null) {
+        	while (fast.next != null && fast.val == fast.next.val) {
+         		fast = fast.next;    //while loop to find the last node of the dups.
+        	}
+        	if (slow.next != fast) { //duplicates detected.
+        		slow.next = fast.next; //remove the dups.
+        		fast = slow.next;     //reposition the fast pointer.
+        	} else { //no dup, move down both pointer.
+        		slow = slow.next;
+        		fast = fast.next;
+        	}
+        	
         }
-
-        ListNode start = new ListNode(0);
-        start.next = head;
-        ListNode dummyNode = start;
-        
-        while(head != null) {
-            while (head.next != null && head.val == head.next.val) {
-                head = head.next;
-            }
-            if(start.next == head) {
-                start = head.next;
-            }else {
-                start.next = head.next;
-            }
-            head = head.next;
-        }
-
-        return dummyNode.next;
-    }
+        return dummy.next;
+    } 
 }
