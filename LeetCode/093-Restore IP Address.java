5f96483f266b754fc77c7e20002fe4effbdb9cdf
Dean <dwen@cmu.edu>
2016-11-07 00:20:44 -0800

    Update 093-Restore IP Address.java

--- LeetCode/093-Restore IP Address.java
+++ LeetCode/093-Restore IP Address.java
@@ -1,36 +1,26 @@
 public class Solution {
     public List<String> restoreIpAddresses(String s) {
-        List<String> result = new ArrayList<String>();
-        if (s.length() < 4 || s.length() > 12) {
-            return result;
-        }
-        dfs(s, "", result, 0);
-        return result;
-    }
-    
-    private void dfs(String s, String item, List<String> result, int start) {
-        if (start == 3 && isValid(s)) {
-            result.add(item + s);
-            return;
-        }
-        
-        for (int i = start; i < 3 && i < s.length() - 1; i++) {
-            String temp = s.substring(0, i + 1);
-            if (isValid(temp)) {
-                dfs(s.substring(i + 1, s.length()), item + temp + '.', result, i + 1);
+        List<String> res = new ArrayList<String>();
+        int len = s.length();
+        for(int i = 1; i < 4 && i < len - 2; i++){
+            for(int j = i + 1; j < i + 4 && j < len - 1; j++){
+                for(int k = j + 1; k < j + 4 && k < len; k++){
+                    String s1 = s.substring(0, i);
+                    String s2 = s.substring(i, j);
+                    String s3 = s.substring(j, k);
+                    String s4 = s.substring(k, len);
+                    if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
+                        res.add(s1+"."+s2+"."+s3+"."+s4);
+                    }
+                }
             }
         }
+        return res;
     }
-    
-    private boolean isValid(String s) {
-        if (s.charAt(0) == '0') {
-            return s.equals("0");
-        }
-        int num = Integer.parseInt(s);
-        if (num > 0 && num <= 255) {
-            return true;
-        }else {
+    public boolean isValid(String s) {
+        if(s.length() > 3 || s.length() == 0 || (s.charAt(0) == '0' && s.length() > 1) || Integer.parseInt(s) > 255) {
             return false;
         }
+        return true;
     }
 }
